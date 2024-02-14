7602804545bccf1edb760b7a766fd738161e4475
yerbolat <erbolatt@live.com>
2024-02-14 14:02:17 +0600

    Initial commit

---
+++ src/main/java/com/yerokha/neotour/NeoTourApplication.java
@@ -0,0 +1,13 @@
+package com.yerokha.neotour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoTourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yerokha/neotour/NeoTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yerokha.neotour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
